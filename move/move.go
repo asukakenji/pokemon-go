40a2550dc37d7780ca0774e2b022b10ea6a8de87
Siu Ching Pong -Asuka Kenji- <asukakenji@users.noreply.github.com>
2016-07-17 10:52:31 +0800

    Added the 'Move' types

---
+++ move/move.go
@@ -0,0 +1,192 @@
+package move
+
+import (
+	typ "github.com/asukakenji/pokemon-go/type"
+)
+
+// Move
+type Move interface {
+	Id() int
+	Type() typ.Type
+	Damage() int
+	EnergyRequired() int
+}
+
+type StandardMove int16
+
+const (
+	_ StandardMove = iota
+	Acid
+	Bite
+	Bubble
+	BugBite
+	BulletPunch
+	Confusion
+	DragonBreath
+	Ember
+	FeintAttack
+	FireFang
+	FrostBreath
+	FuryCutter
+	IceShard
+	KarateChop
+	Lick
+	LowKick
+	MetalClaw
+	MudShot
+	MudSlap
+	Peck
+	PoisonJab
+	PoisonSting
+	Pound
+	PsychoCut
+	QuickAttack
+	RazorLeaf
+	RockSmash
+	RockThrow
+	Scratch
+	ShadowClaw
+	Spark
+	Splash
+	SteelWing
+	SuckerPunch
+	Tackle
+	ThunderShock
+	VineWhip
+	WaterGun
+	WingAttack
+	ZenHeadbutt
+)
+
+//go:generate stringer -type=StandardMove
+
+func (m StandardMove) Id() int {
+	return int(m)
+}
+
+func (m StandardMove) Type() typ.Type {
+	return m.self()._type
+}
+
+func (m StandardMove) Damage() int {
+	return int(m.self().damage)
+}
+
+func (m StandardMove) EnergyRequired() int {
+	return 0
+}
+
+func (m StandardMove) self() *_standard_move {
+	return _standard_moves[m.Id()]
+}
+
+type SpecialMove int16
+
+const (
+	_ SpecialMove = iota
+	AerialAce
+	AirCutter
+	AncientPower
+	AquaJet
+	AquaTail
+	Blizzard
+	BodySlam
+	BoneClub
+	BrickBreak
+	Brine
+	BubbleBeam
+	BugBuzz
+	Bulldoze
+	CrossChop
+	CrossPoison
+	DarkPulse
+	DazzlingGleam
+	Dig
+	DisarmingVoice
+	Discharge
+	DragonClaw
+	DragonPulse
+	DrainingKiss
+	DrillPeck
+	DrillRun
+	Earthquake
+	FireBlast
+	FirePunch
+	FlameBurst
+	FlameCharge
+	FlameWheel
+	Flamethrower
+	FlashCannon
+	GunkShot
+	HeatWave
+	HornAttack
+	Hurricane
+	HydroPump
+	HyperBeam
+	HyperFang
+	IceBeam
+	IcePunch
+	IcyWind
+	IronHead
+	LeafBlade
+	LowSweep
+	MagnetBomb
+	Megahorn
+	Moonblast
+	MudBomb
+	NightSlash
+	OminousWind
+	PetalBlizzard
+	PlayRough
+	PoisonFang
+	PowerGem
+	PowerWhip
+	Psybeam
+	Psychic
+	Psyshock
+	RockSlide
+	RockTomb
+	Scald
+	SeedBomb
+	ShadowBall
+	SignalBeam
+	Sludge
+	SludgeBomb
+	SludgeWave
+	SolarBeam
+	Stomp
+	StoneEdge
+	Struggle
+	Submission
+	Swift
+	Thunder
+	ThunderPunch
+	Thunderbolt
+	Twister
+	ViceGrip
+	WaterPulse
+	Wrap
+	X_Scissor
+)
+
+//go:generate stringer -type=SpecialMove
+
+func (m SpecialMove) Id() int {
+	return int(m)
+}
+
+func (m SpecialMove) Type() typ.Type {
+	return m.self()._type
+}
+
+func (m SpecialMove) Damage() int {
+	return int(m.self().damage)
+}
+
+func (m SpecialMove) EnergyRequired() int {
+	return int(m.self().energyRequired)
+}
+
+func (m SpecialMove) self() *_special_move {
+	return _special_moves[m.Id()]
+}
