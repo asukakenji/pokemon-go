939ab3514bffd6a0d5393c338f75fe1c9e4277f3
Siu Ching Pong -Asuka Kenji- <asukakenji@users.noreply.github.com>
2016-07-25 18:17:31 +0800

    Updated Pokemon data

--- pokemon/pokemon_data.go
+++ pokemon/pokemon_data.go
@@ -47,13 +47,13 @@ var pokemons = [...]*_pokemon{
 	&_pokemon{Raticate, Rattata, -1, -1, t.Normal, t_, 18.5, 0.7, Rattata, 1, 0},                 // 020: Raticate (ラッタ), SD: 800, CP: [,,,296,,,], HP: [,,,43,,,]
 	&_pokemon{Spearow, p_, 10, 10, t.Normal, t.Flying, 2.0, 0.3, Spearow, 1, 50},                 // 021: Spearow (オニスズメ), SD: 10:200 / 400, CP: [10,,,63,,,], HP: [10,,,24,,,]
 	&_pokemon{Fearow, Spearow, -1, -1, t.Normal, t.Flying, 38.0, 1.2, Spearow, 1, 0},             // 022: Fearow (オニドリル)
-	&_pokemon{Ekans, p_, -1, -1, t.Poison, t_, 6.9, 2.0, Ekans, 1, 50},                           // 023: Ekans (アーボ)
+	&_pokemon{Ekans, p_, 10, 10, t.Poison, t_, 6.9, 2.0, Ekans, 1, 50},                           // 023: Ekans (アーボ)
 	&_pokemon{Arbok, Ekans, -1, -1, t.Poison, t_, 65.0, 3.5, Ekans, 1, 0},                        // 024: Arbok (アーボック)
 	&_pokemon{Pikachu, p_, 11, 10, t.Electric, t_, 6.0, 0.4, Pikachu, 1, 50},                     // 025: Pikachu (ピカチュウ), 172 -> 025, SD: 200, CP: [11,,,], HP: [10,,,]
 	&_pokemon{Raichu, Pikachu, -1, -1, t.Electric, t_, 30.0, 0.8, Pikachu, 1, 0},                 // 026: Raichu (ライチュウ)
-	&_pokemon{Sandshrew, p_, -1, -1, t.Ground, t_, 12.0, 0.6, Sandshrew, 1, 50},                  // 027: Sandshrew (サンド)
+	&_pokemon{Sandshrew, p_, 10, 10, t.Ground, t_, 12.0, 0.6, Sandshrew, 1, 50},                  // 027: Sandshrew (サンド)
 	&_pokemon{Sandslash, Sandshrew, -1, -1, t.Ground, t_, 29.5, 1.0, Sandshrew, 1, 0},            // 028: Sandslash (サンドパン)
-	&_pokemon{Nidoran_female, p_, -1, -1, t.Poison, t_, 7.0, 0.4, Nidoran_female, 1, 25},         // 029: Nidoran♀ (ニドラン♀)
+	&_pokemon{Nidoran_female, p_, 10, 11, t.Poison, t_, 7.0, 0.4, Nidoran_female, 1, 25},         // 029: Nidoran♀ (ニドラン♀)
 	&_pokemon{Nidorina, Nidoran_female, -1, -1, t.Poison, t_, 20.0, 0.8, Nidoran_female, 1, 100}, // 030: Nidorina (ニドリーナ)
 	&_pokemon{Nidoqueen, Nidorina, -1, -1, t.Poison, t.Ground, 60.0, 1.3, Nidoran_female, 1, 0},  // 031: Nidoqueen (ニドクイン)
 	&_pokemon{Nidoran_male, p_, -1, -1, t.Poison, t_, 9.0, 0.5, Nidoran_male, 1, 25},             // 032: Nidoran♂ (ニドラン♂)
@@ -65,8 +65,8 @@ var pokemons = [...]*_pokemon{
 	&_pokemon{Ninetales, Vulpix, -1, -1, t.Fire, t_, 19.9, 1.1, Vulpix, 1, 0},                    // 038: Ninetales (キュウコン)
 	&_pokemon{Jigglypuff, p_, -1, -1, t.Normal, t.Fairy, 5.5, 0.5, Jigglypuff, 1, 50},            // 039: Jigglypuff (プリン), 174 -> 039, SD: 600, CP: [,,,110,,,], HP: [,,,69,,,]
 	&_pokemon{Wigglytuff, Jigglypuff, -1, -1, t.Normal, t.Fairy, 12.0, 1.0, Jigglypuff, 1, 0},    // 040: Wigglytuff (プクリン)
-	&_pokemon{Zubat, p_, -1, -1, t.Poison, t.Flying, 7.5, 0.8, Zubat, 1, 50},                     // 041: Zubat (ズバット)
-	&_pokemon{Golbat, Zubat, -1, -1, t.Poison, t.Flying, 55.0, 1.6, Zubat, 1, 0},                 // 042: Golbat (ゴルバット)
+	&_pokemon{Zubat, p_, 10, 10, t.Poison, t.Flying, 7.5, 0.8, Zubat, 1, 50},                     // 041: Zubat (ズバット)
+	&_pokemon{Golbat, Zubat, 25, 15, t.Poison, t.Flying, 55.0, 1.6, Zubat, 1, 0},                 // 042: Golbat (ゴルバット)
 	&_pokemon{Oddish, p_, 13, 10, t.Grass, t.Poison, 5.4, 0.5, Oddish, 1, 25},                    // 043: Oddish (ナゾノクサ), SD: 200, CP: [13,,,], HP: [10,,,]
 	&_pokemon{Gloom, Oddish, -1, -1, t.Grass, t.Poison, 8.6, 0.8, Oddish, 1, 100},                // 044: Gloom (クサイハナ)
 	&_pokemon{Vileplume, Gloom, -1, -1, t.Grass, t.Poison, 18.6, 1.2, Oddish, 1, 0},              // 045: Vileplume (ラフレシア)
@@ -74,7 +74,7 @@ var pokemons = [...]*_pokemon{
 	&_pokemon{Parasect, Paras, -1, -1, t.Bug, t.Grass, 29.5, 1.0, Paras, 1, 0},                   // 047: Parasect (パラセクト)
 	&_pokemon{Venonat, p_, -1, -1, t.Bug, t.Poison, 30.0, 1.0, Venonat, 1, 50},                   // 048: Venonat (コンパン)
 	&_pokemon{Venomoth, Venonat, -1, -1, t.Bug, t.Poison, 12.5, 1.5, Venonat, 1, 0},              // 049: Venomoth (モルフォン)
-	&_pokemon{Diglett, p_, -1, -1, t.Ground, t_, 0.8, 0.2, Diglett, 1, 50},                       // 050: Diglett (ディグダ)
+	&_pokemon{Diglett, p_, 10, 10, t.Ground, t_, 0.8, 0.2, Diglett, 1, 50},                       // 050: Diglett (ディグダ)
 	&_pokemon{Dugtrio, Diglett, -1, -1, t.Ground, t_, 33.3, 0.7, Diglett, 1, 0},                  // 051: Dugtrio (ダグトリオ)
 	&_pokemon{Meowth, p_, -1, -1, t.Normal, t_, 4.2, 0.4, Meowth, 1, 50},                         // 052: Meowth (ニャース)
 	&_pokemon{Persian, Meowth, -1, -1, t.Normal, t_, 32.0, 1.0, Meowth, 1, 0},                    // 053: Persian (ペルシアン)
@@ -84,7 +84,7 @@ var pokemons = [...]*_pokemon{
 	&_pokemon{Primeape, Mankey, -1, -1, t.Fighting, t_, 32.0, 1.0, Mankey, 1, 0},                 // 057: Primeape (オコリザル)
 	&_pokemon{Growlithe, p_, -1, -1, t.Fire, t_, 19.0, 0.7, Growlithe, 1, 50},                    // 058: Growlithe (ガーディ)
 	&_pokemon{Arcanine, Growlithe, -1, -1, t.Fire, t_, 155.0, 1.9, Growlithe, 1, 0},              // 059: Arcanine (ウインディ)
-	&_pokemon{Poliwag, p_, -1, -1, t.Water, t_, 12.4, 0.6, Poliwag, 1, 25},                       // 060: Poliwag (ニョロモ)
+	&_pokemon{Poliwag, p_, 10, 10, t.Water, t_, 12.4, 0.6, Poliwag, 1, 25},                       // 060: Poliwag (ニョロモ)
 	&_pokemon{Poliwhirl, Poliwag, -1, -1, t.Water, t_, 20.0, 1.0, Poliwag, 1, 100},               // 061: Poliwhirl (ニョロゾ)
 	&_pokemon{Poliwrath, Poliwhirl, -1, -1, t.Water, t.Fighting, 54.0, 1.3, Poliwag, 1, 0},       // 062: Poliwrath (ニョロボン)
 	&_pokemon{Abra, p_, -1, -1, t.Psychic, t_, 19.5, 0.9, Abra, 1, 25},                           // 063: Abra (ケーシィ), SD: 400, CP: [,,,54,,,], HP: [,,,13,,,]
@@ -103,8 +103,8 @@ var pokemons = [...]*_pokemon{
 	&_pokemon{Golem, Graveler, -1, -1, t.Rock, t.Ground, 300.0, 1.4, Geodude, 1, 0},              // 076: Golem (ゴローニャ)
 	&_pokemon{Ponyta, p_, -1, -1, t.Fire, t_, 30.0, 1.0, Ponyta, 1, 50},                          // 077: Ponyta (ポニータ)
 	&_pokemon{Rapidash, Ponyta, -1, -1, t.Fire, t_, 95.0, 1.7, Ponyta, 1, 0},                     // 078: Rapidash (ギャロップ)
-	&_pokemon{Slowpoke, p_, -1, -1, t.Water, t.Psychic, 36.0, 1.2, Slowpoke, 1, 50},              // 079: Slowpoke (ヤドン)
-	&_pokemon{Slowbro, Slowpoke, -1, -1, t.Water, t.Psychic, 78.5, 1.6, Slowpoke, 1, 0},          // 080: Slowbro (ヤドラン)
+	&_pokemon{Slowpoke, p_, 15, 17, t.Water, t.Psychic, 36.0, 1.2, Slowpoke, 1, 50},              // 079: Slowpoke (ヤドン)
+	&_pokemon{Slowbro, Slowpoke, 34, 19, t.Water, t.Psychic, 78.5, 1.6, Slowpoke, 1, 0},          // 080: Slowbro (ヤドラン)
 	&_pokemon{Magnemite, p_, -1, -1, t.Electric, t.Steel, 6.0, 0.3, Magnemite, 1, 50},            // 081: Magnemite (コイル), SD: 200, CP: [,,,38,,,], HP: [,,,10,,,]
 	&_pokemon{Magneton, Magnemite, -1, -1, t.Electric, t.Steel, 60.0, 1.0, Magnemite, 1, 0},      // 082: Magneton (レアコイル)
 	&_pokemon{Farfetch_d, p_, -1, -1, t.Normal, t.Flying, 15.0, 0.8, Farfetch_d, 1, 0},           // 083: Farfetch'd (カモネギ)
@@ -144,7 +144,7 @@ var pokemons = [...]*_pokemon{
 	&_pokemon{Seadra, Horsea, -1, -1, t.Water, t_, 25.0, 1.2, Horsea, 1, 0},                      // 117: Seadra (シードラ)
 	&_pokemon{Goldeen, p_, -1, -1, t.Water, t_, 15.0, 0.6, Goldeen, 1, 50},                       // 118: Goldeen (トサキント)
 	&_pokemon{Seaking, Goldeen, -1, -1, t.Water, t_, 39.0, 1.3, Goldeen, 1, 0},                   // 119: Seaking (アズマオウ)
-	&_pokemon{Staryu, p_, 11, 10, t.Water, t_, 34.5, 0.8, Staryu, 1, 50},                         // 120: Staryu (ヒトデマン), SD: 11:200 / 60:400, CP: [11,,,60,,,], HP: [10,,,15,,,]
+	&_pokemon{Staryu, p_, 12, 10, t.Water, t_, 34.5, 0.8, Staryu, 1, 50},                         // 120: Staryu (ヒトデマン), SD: 11:200 / 60:400, CP: [11,,,60,,,], HP: [10,,,15,,,]
 	&_pokemon{Starmie, Staryu, -1, -1, t.Water, t.Psychic, 80.0, 1.1, Staryu, 1, 0},              // 121: Starmie (スターミー)
 	&_pokemon{Mr_Mime, p_, -1, -1, t.Psychic, t.Fairy, 54.5, 1.3, Mr_Mime, 1, 0},                 // 122: Mr. Mime (バリヤード)
 	&_pokemon{Scyther, p_, -1, -1, t.Bug, t.Flying, 56.0, 1.5, Scyther, 1, 0},                    // 123: Scyther (ストライク)
@@ -153,11 +153,11 @@ var pokemons = [...]*_pokemon{
 	&_pokemon{Magmar, p_, -1, -1, t.Fire, t_, 44.5, 1.3, Magmar, 1, 0},                           // 126: Magmar (ブーバー)
 	&_pokemon{Pinsir, p_, -1, -1, t.Bug, t_, 55.0, 1.5, Pinsir, 1, 0},                            // 127: Pinsir (カイロス)
 	&_pokemon{Tauros, p_, -1, -1, t.Normal, t_, 88.4, 1.4, Tauros, 1, 0},                         // 128: Tauros (ケンタロス)
-	&_pokemon{Magikarp, p_, -1, -1, t.Water, t_, 10.0, 0.9, Magikarp, 1, 400},                    // 129: Magikarp (コイキング)
+	&_pokemon{Magikarp, p_, 10, 10, t.Water, t_, 10.0, 0.9, Magikarp, 1, 400},                    // 129: Magikarp (コイキング)
 	&_pokemon{Gyarados, Magikarp, -1, -1, t.Water, t.Flying, 235.0, 6.5, Magikarp, 1, 0},         // 130: Gyarados (ギャラドス)
 	&_pokemon{Lapras, p_, -1, -1, t.Water, t.Ice, 220.0, 2.5, Lapras, 1, 0},                      // 131: Lapras (ラプラス)
 	&_pokemon{Ditto, p_, -1, -1, t.Normal, t_, 4.0, 0.3, Ditto, 1, 0},                            // 132: Ditto (メタモン)
-	&_pokemon{Eevee, p_, -1, -1, t.Normal, t_, 6.5, 0.3, Eevee, 1, 25},                           // 133: Eevee (イーブイ)
+	&_pokemon{Eevee, p_, 14, 11, t.Normal, t_, 6.5, 0.3, Eevee, 1, 25},                           // 133: Eevee (イーブイ)
 	&_pokemon{Vaporeon, Eevee, -1, -1, t.Water, t_, 29.0, 1.0, Eevee, 1, 0},                      // 134: Vaporeon (シャワーズ)
 	&_pokemon{Jolteon, Eevee, -1, -1, t.Electric, t_, 24.5, 0.8, Eevee, 1, 0},                    // 135: Jolteon (サンダース)
 	&_pokemon{Flareon, Eevee, -1, -1, t.Fire, t_, 25.0, 0.9, Eevee, 1, 0},                        // 136: Flareon (ブースター)
