711eef603bb70add21f5b6a961557535d1d926d5
Siu Ching Pong -Asuka Kenji- <asukakenji@users.noreply.github.com>
2016-07-30 09:19:18 +0800

    Added ShortName()

--- lang/language.go
+++ lang/language.go
@@ -50,3 +50,37 @@ func (l Language) LocalName() string {
 		return ""
 	}
 }
+
+// Reference:
+// http://publications.europa.eu/mdr/resource/authority/multilingual/xml/multilingual.xml
+
+func (l Language) ShortName() string {
+	switch l {
+	case Japanese:
+		return "日"
+	case English:
+		return "Eng"
+	case French:
+		return "Fra"
+	case German:
+		return "Deu"
+	case Italian:
+		return "Ita"
+	case Korean:
+		return "한"
+	case Spanish:
+		return "Spa"
+	case ChineseSimplified:
+		return "简(官)"
+	case ChineseTraditional:
+		return "繁(官)"
+	case ChineseChina:
+		return "简(中)"
+	case ChineseHongKong:
+		return "繁(港)"
+	case ChineseTaiwan:
+		return "繁(台)"
+	default:
+		return ""
+	}
+}
