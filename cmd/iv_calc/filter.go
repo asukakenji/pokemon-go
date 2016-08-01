c84b75a9203febae43ae5fe5aef3e69cd6483b21
Siu Ching Pong -Asuka Kenji- <asukakenji@users.noreply.github.com>
2016-08-02 01:37:58 +0800

    Rewritten (intermediate)

---
+++ cmd/iv_calc/filter.go
@@ -0,0 +1,208 @@
+package main
+
+import (
+	"math"
+	"sort"
+
+	"github.com/asukakenji/pokemon-go/lv"
+	"github.com/asukakenji/pokemon-go/pokemon"
+)
+
+type Spec struct {
+	iv pokemon.IndividualValues
+	lvl lv.Level
+}
+
+type PokemonFilter struct {
+	pkm  pokemon.Pokemon
+	specs []Spec
+}
+
+func NewPokemonFilter() *PokemonFilter {
+	filter := &PokemonFilter{pokemon.None, nil}
+	filter.Reset()
+	return filter
+}
+
+func (filter *PokemonFilter) Reset() {
+	ivs := make([]pokemon.IndividualValues, 0, 16*16*16)
+	for stamina := 0; stamina <= 15; stamina++ {
+		for attack := 0; attack <= 15; attack++ {
+			for defense := 0; defense <= 15; defense++ {
+				ivs = append(ivs, pokemon.IndividualValues{stamina, attack, defense})
+			}
+		}
+	}
+
+	lvls := make([]lv.Level, 0, (40-1)*2)
+	for lvl := lv.Level(float32(1.0)); lvl <= 40.0; lvl += 0.5 {
+		lvls = append(lvls, lvl)
+	}
+
+	specs := make([]Spec, 0, len(ivs) * len(lvls))
+	for _, iv := range ivs {
+		for _, lvl := range lvls {
+			specs = append(specs, Spec{iv, lvl})
+		}
+	}
+	filter.specs = specs
+}
+
+func (filter *PokemonFilter) IncrementLvl() {
+	for i := range filter.specs {
+		filter.specs[i].lvl += 0.5
+	}
+}
+
+func (filter *PokemonFilter) SetPokemon(pkm pokemon.Pokemon) {
+	filter.pkm = pkm
+}
+
+func (filter *PokemonFilter) SetCp(cp int) {
+	specs := []Spec{}
+	for _, spec := range filter.specs {
+		cpMin, cpMax, _, _ := filter.pkm.CombatPowerAndHitPoints(spec.iv, spec.lvl)
+		if match(cp, cpMin, cpMax) {
+			specs = append(specs, spec)
+		}
+	}
+	filter.specs = specs
+}
+
+func (filter *PokemonFilter) SetHp(hp int) {
+	specs := []Spec{}
+	for _, spec := range filter.specs {
+		_, _, hpMin, hpMax := filter.pkm.CombatPowerAndHitPoints(spec.iv, spec.lvl)
+		if match(hp, hpMin, hpMax) {
+			specs = append(specs, spec)
+		}
+	}
+	filter.specs = specs
+}
+
+func (filter *PokemonFilter) SetSd(sd int) {
+	specs := []Spec{}
+	for _, spec := range filter.specs {
+		if spec.lvl.StardustToPowerUp() == sd {
+			specs = append(specs, spec)
+		}
+	}
+	filter.specs = specs
+}
+
+func (filter *PokemonFilter) SetCd(cd int) {
+	specs := []Spec{}
+	for _, spec := range filter.specs {
+		if spec.lvl.CandyToPowerUp() == cd {
+			specs = append(specs, spec)
+		}
+	}
+	filter.specs = specs
+}
+
+func (filter *PokemonFilter) SetIsWild(isWild bool) {
+	if !isWild {
+		return
+	}
+	specs := []Spec{}
+	for _, spec := range filter.specs {
+		if spec.lvl.IsInteger() {
+			specs = append(specs, spec)
+		}
+	}
+	filter.specs = specs
+}
+
+func (filter *PokemonFilter) Pokemon() pokemon.Pokemon {
+	return filter.pkm
+}
+
+func (filter *PokemonFilter) MinMaxCpHp() (minCp, maxCp, minHp, maxHp int) {
+	if len(filter.specs) == 0 {
+		return 0, 0, 0, 0
+	}
+
+	minCp, maxCp, minHp, maxHp = 2147483647, 0, 2147483647, 0
+	for _, spec := range filter.specs {
+		cpMin, cpMax, hpMin, hpMax := filter.pkm.CombatPowerAndHitPoints(spec.iv, spec.lvl)
+		if cpMin < minCp {
+			minCp = cpMin
+		}
+		if cpMax > maxCp {
+			maxCp = cpMax
+		}
+		if hpMin < minHp {
+			minHp = hpMin
+		}
+		if hpMax > maxHp {
+			maxHp = hpMax
+		}
+	}
+	return
+}
+
+func (filter *PokemonFilter) MinMaxSdCd() (minSd, maxSd, minCd, maxCd int, sds, cds []int) {
+	if len(filter.specs) == 0 {
+		return 0, 0, 0, 0, []int{}, []int{}
+	}
+
+	minSd, maxSd, minCd, maxCd = 2147483647, 0, 2147483647, 0
+	sds, cds = []int{}, []int{}
+	sdMap, cdMap := map[int]bool{}, map[int]bool{}
+	for _, spec := range filter.specs {
+		sd, cd := spec.lvl.StardustAndCandy()
+		if !sdMap[sd] {
+			sds = append(sds, sd)
+			sdMap[sd] = true
+		}
+		if !cdMap[cd] {
+			cds = append(cds, cd)
+			cdMap[cd] = true
+		}
+		if sd < minSd {
+			minSd = sd
+		}
+		if sd > maxSd {
+			maxSd = sd
+		}
+		if cd < minCd {
+			minCd = cd
+		}
+		if cd > maxCd {
+			maxCd = cd
+		}
+	}
+	sort.Ints(sds)
+	sort.Ints(cds)
+	return
+}
+
+func (filter *PokemonFilter) MinMaxLvl() (minLvl, maxLvl lv.Level) {
+	if len(filter.specs) == 0 {
+		return lv.Level(0.0), lv.Level(0.0)
+	}
+
+	minLvl, maxLvl = lv.Level(math.Inf(1)), lv.Level(0)
+	for _, spec := range filter.specs {
+		if spec.lvl < minLvl {
+			minLvl = spec.lvl
+		}
+		if spec.lvl > maxLvl {
+			maxLvl = spec.lvl
+		}
+	}
+	return
+}
+
+func (filter *PokemonFilter) ForEach(consumer func(pokemon.IndividualValues, lv.Level)) {
+	for _, spec := range filter.specs {
+		consumer(spec.iv, spec.lvl)
+	}
+}
+
+func match(value, min, max int) bool {
+	if max == min || max == min+1 {
+		return min <= value && value <= max
+	}
+	return min < value && value < max
+}
--- cmd/iv_calc/main.go
+++ cmd/iv_calc/main.go
@@ -13,11 +13,6 @@ import (
 	"github.com/asukakenji/pokemon-go/pokemon"
 )
 
-type spec struct {
-	pokemon.IndividualValues
-	Level lv.Level
-}
-
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 
@@ -25,112 +20,114 @@ func main() {
 	lang := cmd.ReadLanguage(reader)
 
 	mode := "N" // N = New, P = Power Up, E = Evolve
+	filter := NewPokemonFilter()
 	pkm := pokemon.None
 	cp := 0
 	hp := 0
 	sd := 0
 	cd := 0
-	wild := true
-	ivs := []pokemon.IndividualValues(nil)
+	isWild := true
 
 	for {
 		// Select Pokémon
 		switch mode {
 		case "N":
-			pkm = cmd.ReadPokemon(lang, reader, pkm)
+			// Reset pkm, specs
+			pkm = cmd.ReadPokemon(lang, reader, pkm, nil)
 			fmt.Printf("#%03d (%s)\n", pkm.Id(), pkm.LocalName(lang))
+			filter.Reset()
+			filter.SetPokemon(pkm)
 		case "P":
-			// Keep previous value
+			// Keep pkm; Increment specs.lvl
+			filter.IncrementLvl()
 		case "E":
-			// TODO: Write this!
+			// Keep specs; Reset pkm
 			pkmIterable := pkm.EvolveTo()
 
 			// This should be implemented as ToSlice()
 			pkms := make([]pokemon.Pokemon, 0)
-			pkmIterable.ForEach(func (p pokemon.Pokemon) {
+			pkmIterable.ForEach(func(p pokemon.Pokemon) {
 				pkms = append(pkms, p)
 			})
 
+			// TODO: Display choices on every iteration
 			for _, p := range pkms {
-				fmt.Println("%d: %s\n", p.Id(), p.LocalName(lang))
+				fmt.Printf("%d: %s\n", p.Id(), p.LocalName(lang))
 			}
 			fmt.Println()
 
-			// TODO: Add valid list
-			pkm = cmd.ReadPokemon(lang, reader, pkm)
+			for _, p := range pkms {
+				pkm = p
+				break
+			}
+
+			pkm = cmd.ReadPokemonWithChoices(lang, reader, pkm, pkms...)
+			filter.SetPokemon(pkm)
 		default:
 			panic("Invalid mode")
 		}
 
 		// Combat Power
-		// TODO: Add valid range
-		cp = cmd.ReadInt(reader, "CP [%d]: ", cp, nil)
+		minCp, maxCp, _, _ := filter.MinMaxCpHp()
+		if !match(cp, minCp, maxCp) {
+			cp = minCp
+		}
+		cpMsg := fmt.Sprintf("CP (%d ~ %d) [%%d]: ", minCp, maxCp)
+		cp = cmd.ReadIntInRange(reader, cpMsg, cp, minCp, maxCp)
+		filter.SetCp(cp)
 
 		// Hit Points
-		// TODO: Add valid range
-		hp = cmd.ReadInt(reader, "HP [%d]: ", hp, nil)
+		_, _, minHp, maxHp := filter.MinMaxCpHp()
+		if !match(hp, minHp, maxHp) {
+			hp = minHp
+		}
+		hpMsg := fmt.Sprintf("HP (%d ~ %d) [%%d]: ", minHp, maxHp)
+		hp = cmd.ReadIntInRange(reader, hpMsg, hp, minHp, maxHp)
+		filter.SetHp(hp)
 
 		// Stardust
-		// TODO: Add valid list
-		sd = cmd.ReadInt(reader, "Stardust [%d]: ", sd, nil)
+		minSd, maxSd, _, _, sds, _ := filter.MinMaxSdCd()
+		if !match(sd, minSd, maxSd) {
+			sd = minSd
+		}
+		sdMsg := fmt.Sprintf("Stardust (%d ~ %d) [%%d]: ", minSd, maxSd)
+		sd = cmd.ReadIntWithChoices(reader, sdMsg, sd, sds...)
+		filter.SetSd(sd)
 
 		// Candy
-		// TODO: Add valid list
-		lvls_temp := lv.LevelsByStardust(sd)
-		for _, lvl := range lvls_temp {
-			cd = lvl.CandyToPowerUp()
-			break
+		_, _, minCd, maxCd, _, cds := filter.MinMaxSdCd()
+		if !match(cd, minCd, maxCd) {
+			cd = minCd
 		}
-		cd = cmd.ReadInt(reader, "Candy (0 = auto) [%d]: ", 0, nil)
+		cdMsg := fmt.Sprintf("Candy (%d ~ %d) [%%d]: ", minCd, maxCd)
+		cd = cmd.ReadIntWithChoices(reader, cdMsg, cd, cds...)
+		filter.SetCd(cd)
 
 		// Wild
 		switch mode {
 		case "N":
-			wildString := cmd.ReadStringWithChoices(reader, "Wild? Yes (Y) / No (N) [%s]: ", "Y", "Y", "N")
-			wild = strings.ToUpper(wildString) != "N"
+			isWildString := strings.ToUpper(cmd.ReadStringWithChoices(reader, "Wild? Yes (Y) / No (N) [%s]: ", "Y", "Y", "N"))
+			isWild = (isWildString != "N")
 		case "P":
-			wild = false
+			isWild = false
 		case "E":
 			// Keep previous value
 		default:
 			panic("Invalid mode")
 		}
+		filter.SetIsWild(isWild)
+		fmt.Println()
 
-		specs := make([]spec, 0)
-
-		if mode == "N" {
-			ivs = allIVs()
-		} else {
-			// Keep previous value
-		}
-
-		lvls := lv.LevelsByStardustAndCandy(sd, cd)
-		ivs2 := make([]pokemon.IndividualValues, 0)
-		for _, iv := range ivs {
-			isMatched := false
-			for _, lvl := range lvls {
-				if wild && !lvl.IsInteger() {
-					continue
-				}
-				if matchCpAndHp(pkm, iv, lvl, cp, hp) {
-					specs = append(specs, spec{iv, lvl})
-					isMatched = true
-				}
-			}
-			if isMatched {
-				ivs2 = append(ivs2, iv)
-			}
-		}
-		ivs = ivs2
-		printSpecs(lang, pkm, cp, hp, sd, cd, wild, specs)
+		printInput(lang, pkm, cp, hp, sd, cd, isWild)
+		printSpecs(lang, filter)
 
 		// Mode
-		mode = cmd.ReadStringWithChoices(reader, "New (N) / Power Up (P) / Evolve (E) [%s]: ", "N", "N", "P", "E")
+		mode = strings.ToUpper(cmd.ReadStringWithChoices(reader, "New (N) / Power Up (P) / Evolve (E) [%s]: ", "N", "N", "P", "E"))
+		fmt.Println()
 	}
 }
 
-func printSpecs(lang language.Language, pkm pokemon.Pokemon, cp, hp, sd, cd int, wild bool, specs []spec) {
-	fmt.Println()
+func printInput(lang language.Language, pkm pokemon.Pokemon, cp, hp, sd, cd int, isWild bool) {
 	fmt.Printf("%s\n", "Input")
 	fmt.Printf("%s\n", "-----")
 	fmt.Printf("Pokémon: #%03d (%s)\n", pkm.Id(), pkm.LocalName(lang))
@@ -138,23 +135,26 @@ func printSpecs(lang language.Language, pkm pokemon.Pokemon, cp, hp, sd, cd int,
 	fmt.Printf("HP: %d\n", hp)
 	fmt.Printf("Stardust: %d\n", sd)
 	fmt.Printf("Candy: %d\n", cd)
-	wildString := ""
-	if wild {
-		wildString = "Yes"
+	isWildString := ""
+	if isWild {
+		isWildString = "Yes"
 	} else {
-		wildString = "No"
+		isWildString = "No"
 	}
-	fmt.Printf("Wild: %s\n", wildString)
+	fmt.Printf("Wild: %s\n", isWildString)
 	fmt.Printf("%s\n", "-----")
 	fmt.Println()
-	count := len(specs)
+}
+
+func printSpecs(lang language.Language, filter *PokemonFilter) {
+	count := 0
 	perfectionSum := 0.0
 	perfectionMin := 100.0
 	perfectionMax := 0.0
 	fmt.Printf("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n", "Level", "Attack", "Defense", "Stamina", "CP Min", "CP Max", "HP Min", "HP Max", "Perfection")
 	fmt.Printf("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n", "-----", "------", "-------", "-------", "------", "------", "------", "------", "----------")
-	for _, spec := range specs {
-		perfection := float64(spec.Attack+spec.Defense+spec.Stamina) * 100.0 / 45.0
+	filter.ForEach(func(iv pokemon.IndividualValues, lvl lv.Level) {
+		perfection := float64(iv.Attack+iv.Defense+iv.Stamina) * 100.0 / 45.0
 		perfectionSum += perfection
 		if perfection < perfectionMin {
 			perfectionMin = perfection
@@ -162,9 +162,10 @@ func printSpecs(lang language.Language, pkm pokemon.Pokemon, cp, hp, sd, cd int,
 		if perfection > perfectionMax {
 			perfectionMax = perfection
 		}
-		cpMin, cpMax, hpMin, hpMax := pkm.RawCombatPowerAndHitPoints(spec.IndividualValues, spec.Level)
-		fmt.Printf("%.1f\t%d\t%d\t%d\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f%%\n", spec.Level, spec.Attack, spec.Defense, spec.Stamina, cpMin, cpMax, hpMin, hpMax, perfection)
-	}
+		cpMin, cpMax, hpMin, hpMax := filter.Pokemon().RawCombatPowerAndHitPoints(iv, lvl)
+		fmt.Printf("%.1f\t%d\t%d\t%d\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f%%\n", lvl, iv.Attack, iv.Defense, iv.Stamina, cpMin, cpMax, hpMin, hpMax, perfection)
+		count++
+	})
 	fmt.Printf("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n", "-----", "------", "-------", "-------", "------", "------", "------", "------", "----------")
 	fmt.Printf("Number of results: %d\n", count)
 	if count == 0 {
@@ -173,32 +174,4 @@ func printSpecs(lang language.Language, pkm pokemon.Pokemon, cp, hp, sd, cd int,
 		fmt.Printf("Perfection (Min, Avg, Max): %f%%, %f%%, %f%%\n", perfectionMin, perfectionSum/float64(count), perfectionMax)
 	}
 	fmt.Println()
-}
-
-func matchCpAndHp(pkm pokemon.Pokemon, iv pokemon.IndividualValues, lvl lv.Level, cp, hp int) bool {
-	cpMin, cpMax, hpMin, hpMax := pkm.CombatPowerAndHitPoints(iv, lvl)
-	if lvl.IsInteger() {
-		return cpMin == cp && hpMin == hp
-	}
-	return cpMin < cp && cp < cpMax && hpMin < hp && hp < hpMax
-}
-
-func allLevels() []lv.Level {
-	result := make([]lv.Level, (40-1)*2)
-	for level := lv.Level(float32(1.0)); level <= 40.0; level += 0.5 {
-		result = append(result, level)
-	}
-	return result
-}
-
-func allIVs() []pokemon.IndividualValues {
-	result := make([]pokemon.IndividualValues, 0, 16*16*16)
-	for stamina := 0; stamina <= 15; stamina++ {
-		for attack := 0; attack <= 15; attack++ {
-			for defense := 0; defense <= 15; defense++ {
-				result = append(result, pokemon.IndividualValues{stamina, attack, defense})
-			}
-		}
-	}
-	return result
 }
